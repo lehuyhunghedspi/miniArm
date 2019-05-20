e65588e4ff4817034755b7f43da7a2c3e433ceae
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-05-20 20:54:31 +0700

    add setup

--- sources/PS2_controll/MeArm.ino
+++ sources/PS2_controll/MeArm.ino
@@ -52,3 +52,67 @@ const int MAX_SIZE_ARR = 20;
 const int num_servo = 4;
 byte scripts[MAX_SIZE_ARR][num_servo] = {{90,90,90,90}};
 int arr_size = 1;
+
+void setup() {
+
+  Serial.begin(57600);
+
+  delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
+  
+  //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
+
+  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+  error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+
+  if (error == 0) {
+    Serial.print("Found Controller, configured successful ");
+    Serial.print("pressures = ");
+    if (pressures)
+      Serial.println("true ");
+    else
+      Serial.println("false");
+    Serial.print("rumble = ");
+    if (rumble)
+      Serial.println("true)");
+    else
+      Serial.println("false");
+    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+    Serial.println("holding L1 or R1 will print out the analog stick values.");
+    Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
+  }
+  else if (error == 1)
+    Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+
+  else if (error == 2)
+    Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+
+  else if (error == 3)
+    Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+
+  //  Serial.print(ps2x.Analog(1), HEX);
+
+  type = ps2x.readType();
+  switch (type) {
+    case 0:
+      Serial.print("Unknown Controller type found ");
+      break;
+    case 1:
+      Serial.print("DualShock Controller found ");
+      break;
+    case 2:
+      Serial.print("GuitarHero Controller found ");
+      break;
+    case 3:
+      Serial.print("Wireless Sony DualShock Controller found ");
+      break;
+  }
+
+
+  /*----------Servo--------*/
+  baseServo.attach(baseServoPin);//0-180
+  elbowServo.attach(elbowServoPin);//0-80-150
+  shoulderServo.attach(shoulderServoPin);//0-100
+  gripperServo.attach(gripperServoPin);//0-100
+  
+}
+
